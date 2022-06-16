561693cbc6f6392a7e3ddf34118b148b1beefcde
sivaramakrishnanelango <sivaram.e98@gmail.com>
2022-06-16 11:16:38 +0530

    some code are added

--- src/com/redmi/redmiTv.java
+++ src/com/redmi/redmiTv.java
@@ -2,4 +2,16 @@ package com.redmi;
 
 public class redmiTv {
 
+	private void redmiAcces() {
+		System.out.println("i like tv");
+
+	}
+	private void redmimobile() {
+		System.out.println("I like using mobile");
+
+	}
+	private void redmiHeater() {
+		System.out.println("heatre is used");
+
+	}
 }
